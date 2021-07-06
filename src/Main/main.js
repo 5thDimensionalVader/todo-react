36d8e7eed930ae31cb167576ea602eaf8f900e7b
5thDimensionalVader <5thDimensionalVader@protonmail.com>
2021-07-06 23:31:32 +0000

    todo-react first commit

---
+++ src/Main/main.js
@@ -0,0 +1,11 @@
+import './main.css';
+
+const Main = () => {
+  return ( 
+    <div>
+      <h1>Main Page</h1>
+    </div>
+   );
+}
+ 
+export default Main;
---
+++ src/Views/view.js
@@ -0,0 +1,11 @@
+import './view.css';
+
+const View = () => {
+  return ( 
+    <div>
+      <h1>View Page</h1>
+    </div>
+   );
+}
+ 
+export default View;
